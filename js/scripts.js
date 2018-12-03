63c30acf9a37095faa66eb17906ee2ce3e58b2b2
joseck ogachi <jogachi4@gmail.com>
2018-12-03 11:01:44 +0300

    add changes to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,26 @@
-function theTriangle(){
-var a = parseInt(document.getElementById('a').value);
-var b = parseInt(document.getElementById('b').value);
-var c = parseint(document.getElementById('c').value);
-var ans = document.getElementById('answerResult');
-if(a+b>c && b+c>a && a+b>c){
-  if(a==b&&c=a){
-    answerResult.textContent='Equilateral:All sides are equal!';
+function press(){
+var add = function(side1,side2,side3) {
+return side1+side2+side3;
+};
+
+{
+var side1 = parseInt(prompt("enter side1:"));
+var side2 = parseInt(prompt("enter side2:"));
+var side3 = parseInt(prompt("enter side3:"));
+var sum = add(side1, side2, side3);
+alert(sum);
+
+if(side1+side2<=side3 && side2+side3<=side1)
+alert('Not a triangle!')
+
+ else if(side1==side2&&side2==side3)
+   alert('Equilateral:All sides are equal!');
+
+  else if(side1===side2||side2===side3)
+    alert('Isosceles:Two sides are equal!');
+
+  else
+    alert('Scalene:No sides are equal!'
+    );
   }
-  else if(a===b||b===c||c===a){
-    answerResult.textContent='Isosceles:Two sides are equal!';
-  }
-  else {
-    answerResult.textContent='Scalene:No sides are equal!';
-  }
-  else {
-    answerResult.textContent='Not a triangle!';
-  }
-  }
+}
